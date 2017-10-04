398be891ff0df8cd8b262982d8d5a3033763ba7e
AfonichevDs <afonichevds@gmail.com>
2017-10-04 17:45:14 +0300

    v.1.0

---
+++ Metro/Metro/Graphs.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Metro
+{
+    class Node
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public Node Ancestor { get; set; }
+        public bool IsChecked { get; set; }
+        public Point SPoint { get; set; }
+
+        public Node(string name, int value, Point point, bool ischecked = false)
+        {
+            Name = name;
+            Value = value;
+            IsChecked = ischecked;
+            Ancestor = new Node();
+            SPoint = point;
+        }
+
+        public Node(int value, Point point, bool ischecked = false)
+        {
+            Name = null;
+            Value = value;
+            IsChecked = ischecked;
+            Ancestor = new Node();
+            SPoint = point;
+        }
+        public Node() { }
+    }
+
+    class Edge
+    {
+        public const int Weight = 1;
+
+        public Node FirstNode { get; private set; }
+        public Node LastNode { get; private set; }
+
+        public Edge(Node firstNode, Node lastNode)
+        {
+            LastNode = lastNode;
+            FirstNode = firstNode;
+        }
+    }
+
+    class Dijkstra
+    {
+        public Node[] Nodes { get; private set; }
+        public Edge[] Edges { get; private set; }
+        public Node StartPoint { get; private set; }
+
+        public Dijkstra(Node[] ns, Edge[] eds)
+        {
+            Nodes = ns;
+            Edges = eds;
+        }
+
+        public void RunAlgorithm(Node start)
+        {
+            if (Nodes.Length == 0 || Edges.Length == 0)
+            {
+                throw new Exception();
+            }
+
+            StartPoint = start;
+            OneStep(StartPoint);
+
+            foreach (Node node in Nodes)
+            {
+                Node unchecknode = UncheckedNode();
+
+                if (unchecknode != null)
+                {
+                    OneStep(unchecknode);
+                }
+                else break;
+            }
+        }
+
+        public void OneStep(Node node)
+        {
+            IEnumerable<Node> neighbours = GetNeighbours(node);
+
+            Node minNode = new Node();
+            foreach (var n in neighbours)
+            {
+                if (n.IsChecked == false)
+                {
+                    int newvalue = node.Value + Edge.Weight;
+                    if (n.Value > newvalue)
+                    {
+                        n.Value = newvalue;
+                        n.Ancestor = node;
+                    }
+                }
+            }
+            node.IsChecked = true;
+        }
+
+        public IEnumerable<Node> GetNeighbours(Node node)
+        {
+            IEnumerable<Node> frpoints = from fp in Edges where fp.FirstNode == node select fp.LastNode;
+            IEnumerable<Node> scpoints = from sc in Edges where sc.LastNode == node select sc.FirstNode;
+
+            IEnumerable<Node> totalnodes = frpoints.Concat(scpoints);
+
+            return totalnodes;
+        }
+
+        private Node UncheckedNode()
+        {
+            IEnumerable<Node> unpoints = from ns in Nodes where ns.IsChecked == false select ns;
+
+            if (unpoints.Count() != 0)
+            {
+                int minvalue = unpoints.First().Value;
+                Node minNode = unpoints.First();
+                foreach (var n in unpoints)
+                {
+                    if (n.Value < minvalue)
+                    {
+                        minvalue = n.Value;
+                        minNode = n;
+                    }
+                }
+                return minNode;
+            }
+            return null;
+        }
+
+        public List<Node> MinAlgorithm(Node end)
+        {
+            if (end == null) throw new Exception("end==null");
+            List<Node> path = new List<Node>();
+            Node temp = new Node();
+            temp = end;
+
+            while (temp != this.StartPoint)
+            {
+                path.Add(temp);
+                temp = temp.Ancestor;
+            }
+            path.Add(StartPoint);
+            return path;
+        }
+    }
+}
---
+++ Metro/Metro/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Metro
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StreamReader reader = new StreamReader("Points.txt");
+
+        Dictionary<string, Node> stationsdict = new Dictionary<string, Node>();
+        List<Node> stations = new List<Node>();
+        List<Edge> edges = new List<Edge>();
+
+        public MainWindow()
+        { 
+            InitializeComponent();
+            //MouseMove += OnMouseMove;
+            Closed += (object sender, EventArgs e) => { reader.Close();
+                //writer.Close();
+            };
+           // MouseDown += OnMouseDown;
+        }
+
+        //private void OnMouseMove(object sender, MouseEventArgs e)
+        //{
+          //  Start.Text = String.Format("X: {0} , Y: {1}",e.GetPosition(this).X, e.GetPosition(this).Y);
+        //}
+
+        //private void OnMouseDown(object sender, MouseEventArgs e)
+        //{
+        //    writer.WriteLine(e.GetPosition(this).X);
+        //    writer.WriteLine(e.GetPosition(this).Y);
+        //}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReadPoints();
+        }
+
+        private void Route_Click(object sender, RoutedEventArgs e)
+        {
+            CreateEdges(ref edges);
+
+            var stats = stations.ToArray();
+            var edgs = edges.ToArray();
+
+            Dijkstra findway = new Dijkstra(stats, edgs);
+            stationsdict[Start.Text].Value = 0;
+            findway.RunAlgorithm(stationsdict[Start.Text]);
+
+            List<Node> MyWay = findway.MinAlgorithm(stationsdict[Destination.Text]);
+            IEnumerable<Point> Points = from ps in MyWay select ps.SPoint;
+
+            PathFigure geometry = new PathFigure();
+            geometry.StartPoint = stationsdict[Start.Text].SPoint;
+            geometry.Segments.Add(new PolyLineSegment(Points.Reverse(),false));
+
+            PathGeometry path = FindResource("MetroPath") as PathGeometry;
+            path.Figures.Add(geometry);
+
+            Storyboard story = this.FindResource("MetroAnim") as Storyboard;
+            story.Begin();
+        }
+
+        private void ReadPoints()
+        {
+            while (reader.Peek() != -1)
+            {
+                int x = Convert.ToInt32(reader.ReadLine());
+                int y = Convert.ToInt32(reader.ReadLine());
+
+                stations.Add(new Node(999,new Point(x,y)));
+            }
+            StreamReader reader2 = new StreamReader("StLines.txt");
+            int count = 0;
+            while (reader2.Peek() != -1)
+            {
+                string stname = reader2.ReadLine();
+                stations[count].Name = stname;
+                stationsdict.Add(stname, stations[count]);
+                count++;
+            }
+        }
+
+        private void CreateEdges(ref List<Edge> edges)
+        {
+            for(int i = 1; i < stations.Count;i++)
+            {
+                if (i == 18 || i == 34) continue;
+                edges.Add(new Edge(stations[i - 1], stations[i]));
+            }
+            edges.Add(new Edge(stationsdict["Teatralna"], stationsdict["Zoloti Vorota"]));
+            edges.Add(new Edge(stationsdict["Khreshchatyk"], stationsdict["Maidan Nezalezhnosti"]));
+            edges.Add(new Edge(stationsdict["Palats Sportu"],stationsdict["Ploscha Lva Tolstoho"]));
+        }
+
+    }
+}
